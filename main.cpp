6fb9d28f0cc1cd35a830e2bb6090c2f93f30c1e4
matteoalciati <91663997+matteoalciati@users.noreply.github.com>
2021-12-11 15:55:32 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,29 @@
 #include <iostream>
 
-int main(){
-  return 0;
+using namespace std;
+
+#include "lib.h"
+
+
+int main() {
+    int k=0;
+
+
+    char nome[1][20];
+
+    char listanomi [10][20];
+    for (int i=0; i<10; i++){
+
+        cin >> listanomi[i];
+
+    }
+
+  cin >> nome[0];
+
+if (funzione (nome, listanomi, k) == true){
+    cout << k;
+} else {
+    cout << "non presente";
+}
+    return 0;
 }
