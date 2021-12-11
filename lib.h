5fe17dece4b18122f10facb0b0bf1a0795b4ac23
matteoalciati <91663997+matteoalciati@users.noreply.github.com>
2021-12-11 16:49:49 +0100

    Update lib.h

--- lib.h
+++ lib.h
@@ -1,6 +1,7 @@
-#ifndef MAIN_CPP_LIB_H
-#define MAIN_CPP_LIB_H
 
-#endif //MAIN_CPP_LIB_H
+#ifndef ESERCITAZIONE9_3_LIB_H
+#define ESERCITAZIONE9_3_LIB_H
+
+#endif //ESERCITAZIONE9_3_LIB_H
 
 bool funzione (char nome[1][20],char listanomi [10][20], int &k);
