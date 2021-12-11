f1fda7101a4724208c08d18f9a26e9d7b6f6ba84
matteoalciati <91663997+matteoalciati@users.noreply.github.com>
2021-12-11 18:36:05 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -4,7 +4,7 @@
 bool funzione (char listanomi[10][20],char nome[1][20], int &k ){
     for (int i=0; i<10; i++){
         if (strcmp(listanomi[i], nome[0])==0){
-            k=i+1;
+            k=i;
             return true;
         }
     }
