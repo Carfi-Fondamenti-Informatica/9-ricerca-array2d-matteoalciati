3538584aa721c326489090652d25288dd593df21
matteoalciati <91663997+matteoalciati@users.noreply.github.com>
2021-12-11 18:23:33 +0100

    Update lib.h

--- lib.h
+++ lib.h
@@ -1,7 +1,7 @@
 
-#ifndef ESERCITAZIONE9_3_LIB_H
-#define ESERCITAZIONE9_3_LIB_H
+#ifndef ESERCITAZIONE9_4_LIB_H
+#define ESERCITAZIONE9_4_LIB_H
 
-#endif //ESERCITAZIONE9_3_LIB_H
+#endif //ESERCITAZIONE9_4_LIB_H
 
-bool funzione (char nome[1][20],char listanomi [10][20], int &k);
+bool funzione (char listanomi[10][20],char nome[1][20], int &k );
