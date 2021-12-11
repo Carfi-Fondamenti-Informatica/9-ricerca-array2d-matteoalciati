057f9b7934b098711b436592476595a7a14d650a
matteoalciati <91663997+matteoalciati@users.noreply.github.com>
2021-12-11 16:42:00 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -17,9 +17,26 @@ int main() {
         cin >> listanomi[i];
 
     }
+    
+     for (int i=0; i<10; i++){
+        for (int j=0; j<20;j++){
+            listanomi[i][j];
+            if (listanomi[i][j] >= 65 && listanomi[i][j] <= 90){
+                listanomi[i][j] = listanomi[i][j] + 32;
+            }
+        }
+
+    }
 
 
     cin >> nome[0];
+   
+    for (int j=0; j<20;j++) {
+        listanomi[0][j];
+        if (nome[0][j] >= 65 && nome[0][j] <= 90) {
+            nome[0][j] = nome[0][j] + 32;
+        }
+    }
 
     
 
