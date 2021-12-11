8b29b68ce01cc277f4b62fde9891364741df72e1
matteoalciati <91663997+matteoalciati@users.noreply.github.com>
2021-12-11 18:22:21 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,51 +1,29 @@
 #include <iostream>
+#include "lib.h"
 
 using namespace std;
 
-#include "lib.h"
+
+    int main() {
+        int k = 0;
 
 
-int main() {
-    int k=0;
-    char u;
+        char nome[1][20];
 
+        char listanomi[10][20];
+        for (int i = 0; i < 10; i++) {
 
-    char nome[1][20];
+            cin >> listanomi[i];
 
-    char listanomi [10][20];
-    for (int i=0; i<10; i++){
-
-        cin >> listanomi[i];
-
-    }
-
-
-    for (int i=0; i<10; i++){
-        for (int j=0; j<20;j++){
-            listanomi[i][j];
-            if (listanomi[i][j] >= 65 && listanomi[i][j] <= 90){
-                listanomi[i][j] = listanomi[i][j] + 32;
-            }
         }
 
-    }
+        cin >> nome[0];
 
-
-    cin >> nome[0];
-
-    for (int j=0; j<20;j++) {
-        listanomi[0][j];
-        if (nome[0][j] >= 65 && nome[0][j] <= 90) {
-            nome[0][j] = nome[0][j] + 32;
+        if (funzione(listanomi,nome, k )==true){
+            cout << k ;
+        } else {
+            cout << "non presente";
         }
+
+        return 0;
     }
-
-
-
-    if (funzione (nome, listanomi, k) == true){
-        cout << k;
-    } else {
-        cout << "non presente";
-    }
-    return 0;
-}
