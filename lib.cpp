5e905d6c521742b538d129f7bb9e9e429741989f
matteoalciati <91663997+matteoalciati@users.noreply.github.com>
2021-12-11 16:50:20 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,3 +1,4 @@
+
 #include "lib.h"
 
 bool funzione (char nome[1][20],char listanomi [10][20], int &k){
@@ -5,7 +6,7 @@ bool funzione (char nome[1][20],char listanomi [10][20], int &k){
     for (int i=0; i<10; i++){
         char a=0;
         char b=0;
-        for (int j=0; j<20 and (a==b or a==b+32 or a+32==b or a+32==b+32) ; j++){
+        for (int j=0; j<20 and a==b ; j++){
             a=listanomi[i][j];
             b=nome[0][j];
             h=j;
@@ -20,3 +21,4 @@ bool funzione (char nome[1][20],char listanomi [10][20], int &k){
 
     return false;
 
+}
